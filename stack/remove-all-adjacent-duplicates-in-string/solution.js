c8a30b2ae2f66f8a572410ffbdea74b980aa9520
Crystal Tran <112344007+crystal-tran@users.noreply.github.com>
2024-06-12 13:30:40 -0400

    Create solution.js

---
+++ stack/remove-all-adjacent-duplicates-in-string/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function(s) {
+    const stack = [];
+
+    for(let c of s){
+        if(stack.length && stack[stack.length-1] == c){
+            stack.pop()
+        } else {
+            stack.push(c)
+        }
+    }
+    return stack.join('');
+};
